647e5dca5e7af9f9b6e7db74eb5d7139f3b71098
scott-ha <haneul9209@gmail.com>
2021-04-21 01:27:15 +0900

    HN first commit

---
+++ routes/kakao/kakao_main.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+var myKakao = require("./myKakao");
+
+var kakao, kakao_res, test_text;
+// /api/kakao
+router.post("/", function (req, res, next) {
+  kakao = new myKakao();
+
+  test_text =
+    "알림톡 도착\n[북테온] 판매 도서 정보 안내\n" +
+    "도서명: 삼국유사\n" +
+    "지은이: 일연\n" +
+    "옮긴이: 김원중\n" +
+    "출판사: 민음사\n" +
+    "출판날짜: 2008\n" +
+    "ISBN: 9788937461668\n" +
+    "신규 도서를 확인하세요!\n";
+
+  kakao.setStext = test_text;
+  kakao_res = kakao.SimpleText;
+  console.log(kakao_res);
+  res.json(kakao_res);
+});
+
+module.exports = router;
---
+++ routes/kakao/myKakao.js
@@ -0,0 +1,45 @@
+// 'use strict';
+
+var fs = require("fs");
+var path = require("path");
+var response, response_item, imgpath;
+/* important */
+// kakao response to class!!
+
+module.exports = class myKakao {
+  constructor() {
+    this.data;
+  }
+  set setStext(data) {
+      this.stext = data;
+  }
+  get SimpleText() {
+    response = {
+      "version:": "2.0",
+      template: {
+        outputs: [
+          {
+            simpleText: {
+              text: this.stext,
+            },
+          },
+        ],
+      },
+    };
+    return response
+  }
+};
+
+// img exist?
+module.exports.check_img = function (img) {
+  if (fs.existsSync(path.join(process.cwd(), "public", "img", img + ".jpg"))) {
+    imgpath = encodeURI(img);
+  } else {
+    // need to image change
+    imgpath = encodeURI("gachon");
+  }
+  // image path need to modify
+  imgpath = "https://sg-attend-hnnwp.run.goorm.io/img/" + img + ".jpg";
+  console.log(imgpath);
+  return imgpath;
+};
